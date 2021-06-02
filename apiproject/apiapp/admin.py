a971a96c1c0297f095408256e67bb8086e2c797d
mazharcse <mazhar_cse@yahoo.com>
2021-06-03 03:07:06 +0600

    database/models created and registered

--- apiproject/apiapp/admin.py
+++ apiproject/apiapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from apiapp.models import Country, State, Address
+
+# Register your models here.
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(Address)
--- apiproject/apiapp/models.py
+++ apiproject/apiapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    code = models.CharField(max_length=10)
+
+class State(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+class Address(models.Model):
+    name = models.CharField(max_length=100)
+    house_number = models.CharField(max_length=50)
+    road_number = models.IntegerField()
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
